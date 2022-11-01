d13f9aac719d122b71060526f98e7625f3fa9527
ohsaraho <osg21001@byui.edu>
2022-10-31 20:35:13 -0600

    changed url to render, checked if gets token

--- middleWare/loadUser.js
+++ middleWare/loadUser.js
@@ -9,6 +9,7 @@ const User = db.user;
 const loadUser = async (req, res, next) => {
   // console.log(req.headers.authorization);
   const authZeroUser = fetchAuthZeroUser(req.headers.authorization);
+  // const authZeroUser = req.headers.authorization;
 
   const user = await findOrCreateUser(authZeroUser);
 
@@ -19,9 +20,18 @@ const loadUser = async (req, res, next) => {
 
 const fetchAuthZeroUser = async (authorizationValue) => {
 
+  // It does get the token
+  // console.log("Is token present: ", authorizationValue);
+
+  // It returns the correct url
+  // console.log(config.authorizationHost);
+
   const response = await fetch(`${config.authorizationHost}/userinfo`, {
     headers: { Authorization: authorizationValue },
   });
+
+  // const text = await response.text();
+  // console.log(text);
 
   return response.json();
 };
@@ -30,11 +40,12 @@ const findOrCreateUser = async (authZeroUserJson) => {
   if (!authZeroUserJson) return;
 
   const existingUser = await User.findOne({ identifier: authZeroUserJson.sub });
+  console.log(authZeroUserJson.sub)
 
   if (existingUser) return existingUser;
 
-  const user = new User(req.body);
-  const newUser = await user.create({
+  // const user = new User(req.body);
+  const newUser = await User.save({
     identifier: authZeroUserJson.sub,
     email: authZeroUserJson.email,
     givenName: authZeroUserJson.given_name,
