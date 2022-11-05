25e10456d79fa57092d717cada285580b6c75278
ohsaraho <osg21001@byui.edu>
2022-11-05 13:17:03 -0600

    added console.log in authController and try block

--- controllers/authorizationController.js
+++ controllers/authorizationController.js
@@ -3,6 +3,7 @@ const config = require("../config/OAuth");
 
 const AuthorizationController = {
   login: (req, res) => {
+    console.log("===== In Login Controller =====");
     const authorizationURL = `${
       config.authorizationHost
     }/authorize?response_type=code&client_id=${
@@ -14,26 +15,31 @@ const AuthorizationController = {
     res.redirect(authorizationURL);
   },
   callback: async (req, res) => {
-    const response = await fetch(`${config.authorizationHost}/oauth/token`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: new URLSearchParams({
-        grant_type: "authorization_code",
-        client_id: config.clientID,
-        client_secret: config.clientSecret,
-        redirect_uri: config.redirectUrl,
-        scope: "openid profile email",
-        code: req.query.code,
-      }),
-    }
+    try {
+      console.log("===== In Callback Controller =====");
+      const response = await fetch(`${config.authorizationHost}/oauth/token`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          grant_type: "authorization_code",
+          client_id: config.clientID,
+          client_secret: config.clientSecret,
+          redirect_uri: config.redirectUrl,
+          scope: "openid profile email",
+          code: req.query.code,
+        }),
+      }
   );
 
-  const json = await response.json();
+    const json = await response.json();
 
-  res.json(json);
+    res.json(json);
+  } catch (error) {
+    next(error);
+  }
   },
 };
 
